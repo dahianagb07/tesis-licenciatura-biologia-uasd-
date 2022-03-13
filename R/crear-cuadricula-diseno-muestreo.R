d3bb12ab8237a3fc1b34d244329ddb5c0287b83d
Jose Ramon <joseramon@geografiafisica.org>
2022-03-13 10:55:42 -0400

    ADICIÓN script de QGIS usado para crear malla hexagonal

--- R/crear-cuadricula-diseno-muestreo.R
+++ R/crear-cuadricula-diseno-muestreo.R
@@ -1,7 +1,7 @@
 # Crear cuadrícula para diseño de muestreo
 library(dplyr)
 library(sf)
-parque <- st_read('data/limite-parque.gpkg')
+parque <- st_read('data/limite-parque.gpkg') # Creada en QGIS, ver nota abajo
 cuad <- st_read('data/cuadricula.gpkg')
 plot(parque %>% st_geometry)
 plot(cuad %>% st_geometry, add=T)
@@ -30,3 +30,6 @@ cuad_final <- cuad_final %>% rename(a0_square_meters = AREASQM1)
 plot(parque %>% st_geometry)
 plot(cuad_final %>% st_geometry, add=T)
 # st_write(cuad_final, 'data/cuadricula-final.gpkg')
+
+# Nota: creado mediante QGIS
+# processing.run("native:creategrid", {'TYPE':4,'EXTENT':'402637.28125,403169.15625,2041819.0,2042297.125 [EPSG:32619]','HSPACING':75,'VSPACING':75,'HOVERLAY':5,'VOVERLAY':5,'CRS':QgsCoordinateReferenceSystem('EPSG:32619'),'OUTPUT':'ogr:dbname=\'/home/jr/Documentos/tesis-dahiana/tesis-licenciatura-biologia-uasd-/data/cuadricula.gpkg\' table=\"cuadricula\" (geom)'})
